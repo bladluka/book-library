27132562ad6eef4304e21800b806521caf63a6fc
Łukasz Blada <lukasz.blada@jit.team>
2022-02-01 17:46:34 +0100

    updated database

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestingConfig(Config):
